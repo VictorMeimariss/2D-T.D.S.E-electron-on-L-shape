20d3a91bcdf6c68bbd5d76bae8abff6037701b02
VictorMeimariss <victor_meimaris@outlook.com>
2025-03-24 00:23:24 +0200

    Added L shaped global and l2g
    Added L shaped domain with global coordinates and local coordinates for squared elements, the function also fixes the max_length input as the boundary nodes need to be nodes of these squares for fem to work.

--- Computational_physics/quantum_wavefunction/main.jl
+++ Computational_physics/quantum_wavefunction/main.jl
@@ -1,47 +1,6 @@
-using Gridap
-using Gridap.Geometry
-using Gridap.FESpaces
-using Gridap.CellData
-using Plots
+# Victor Emmanuel Meimaris 23/3/25: Created main.jl for project / excersice for advanced scientific calc.
 
-# 1️⃣ Define the domain (L-shaped region)
-domain = (0, 1, 0, 1)  # A square domain for now (we'll make L-shape later)
+# Domain size
+L = [-1, 1]
 
-# 2️⃣ Generate a rectangular mesh
-model = CartesianDiscreteModel(domain, (20, 20))  # 20x20 grid
-
-# 3️⃣ Define function space
-order = 1  # Linear basis functions (FEM order)
-reffe = ReferenceFE(lagrangian, Float64, order)
-V = TestFESpace(model, reffe, conformity=:H1)
-
-# 4️⃣ Define the PDE: Poisson equation
-Ω = Triangulation(model)
-dΩ = Measure(Ω, 2 * order)
-
-# Weak form of the Poisson equation: ∫(∇u ⋅ ∇v) dΩ = ∫(f ⋅ v) dΩ
-a(u, v) = ∫(∇(u) ⋅ ∇(v)) * dΩ  # Bilinear form
-b(v) = ∫(1 * v) * dΩ  # Linear form (source term f = 1)
-
-# 5️⃣ Solve the FEM system
-op = AffineFEOperator(a, b, V, V)
-uh = solve(op)
-
-# 6️⃣ Extract nodal values for plotting
-# Use Gridap's `get_cell_coordinates` to extract nodal coordinates
-cell_coords = get_cell_coordinates(Ω)
-x = vcat(cell_coords...)  # Flatten the list of coordinates
-
-# Convert VectorValue{2, Float64} to separate arrays for x and y coordinates
-x_coords = [point[1] for point in x]
-y_coords = [point[2] for point in x]
-
-# Evaluate the solution at the nodes
-uh_values = uh.(x)
-
-# 7️⃣ Plot the solution
-plotlyjs()
-scatter(x_coords, y_coords, uh_values, marker_z=uh_values, label="Solution", color=:viridis)
-xlabel!("x")
-ylabel!("y")
-title!("FEM Solution for Poisson Equation")
+# Input for maximum square side length
---
+++ Computational_physics/quantum_wavefunction/test.jl
@@ -0,0 +1,74 @@
+    domain=[-1 ,1]
+    max_length = 1
+    # Define domain length in any axis since x and y have the same domain
+    domain_length = domain[2] - domain[1]
+
+    # This division has to be a multiple of 2 in order for FEM to work so we modify max_length accordingly
+    temp = domain_length / max_length
+    if temp < 2
+        max_length = domain_length / 2
+    elseif temp % 2 != 0
+        temp = round(Int, temp)  # Round to the nearest integer
+        if temp % 2 != 0
+            temp += 1
+        elseif temp == 2 # Added this for occasions where temp rounds to 2 so it needs +2
+            temp += 2
+        end
+        max_length = domain_length / temp
+    end
+    temp = round(Int, temp) # Case for if temp = 2.0
+
+    # Deriving number of squares based on length provided
+    accuracy_level = 2 # Might change these later, put it as input or remove it
+    num_of_squares_y = temp * accuracy_level
+    num_of_squares_x = temp * accuracy_level # This could be different to y if multiple of 2
+
+    # Create square parts of the global matrix and then merge them with v and h cat into one global matrix g
+    a = reshape(1:((num_of_squares_x ÷ 2) + 1) * ((num_of_squares_y ÷ 2) + 1), (num_of_squares_x ÷ 2) + 1, (num_of_squares_y ÷ 2) + 1)
+    a = transpose(a)
+
+    # Creating (n+1, n) sparse matrix for domain outside of Omega and boundaries with it to merge with a
+    b = zeros(Int, (num_of_squares_y ÷ 2) + 1, num_of_squares_x ÷ 2)
+    temp = a[end, end] + 1
+    for i = 1:num_of_squares_x ÷ 2
+        b[end, i] = temp
+        global temp +=1
+    end
+
+    # Merging a and b horizontally to form half of global matrix
+    g = hcat(a, b) 
+
+    # Number of elements and nodes
+    noe = ((num_of_squares_y ÷ 2) * num_of_squares_x) + ((num_of_squares_y ÷ 2) * (num_of_squares_x ÷ 2))  # Number of elements
+    nop = (num_of_squares_x + 1) * (num_of_squares_y + 1) - length(b) + num_of_squares_x ÷ 2 # Number of nodes
+    
+    # Creating rest of the global map and merging it with g
+    # c is the remainder and goes from temp to nop - length(b) + num_of_squares_y ÷ 2 
+    c = reshape(temp:nop, num_of_squares_x + 1, num_of_squares_y ÷ 2)
+    c = transpose(c)
+
+    # Merge c and g into g the global matrix
+    g = vcat(g, c)
+
+    # Generate coordinates
+    x = range(domain[1], domain[2], num_of_squares_x + 1)
+    y = range(domain[1], domain[2], num_of_squares_y + 1)
+
+    # Define local to global map " l2g "
+    l2g = zeros(Int, noe, 4) # Each element has 4 nodes
+    temp = 1
+    # For top left corner of domain
+    for i = 1: num_of_squares_y ÷ 2
+        for j = 1: num_of_squares_x ÷ 2
+            l2g[temp, :] = [g[i, j], g[i, j + 1], g[i + 1, j + 1], g[i + 1, j]]
+            global temp += 1
+        end
+    end
+    # For bottom half
+    for i = (num_of_squares_y ÷ 2) + 1: num_of_squares_y
+        for j = 1: num_of_squares_x
+            l2g[temp, :] = [g[i, j], g[i, j + 1], g[i + 1, j + 1], g[i + 1, j]]
+            global temp += 1
+        end
+    end
+    println(l2g)
