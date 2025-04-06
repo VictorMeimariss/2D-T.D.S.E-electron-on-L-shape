4e7367cfda647b94b262849b43f9cb3a3703d643
VictorMeimariss <victor_meimaris@outlook.com>
2025-04-07 00:55:17 +0300

    Created global stiffness matrix and gauss_quad
    Created global stiffness matrix using gauss quad in 2D and tested it.

--- Computational_physics/quantum_wavefunction/functions.jl
+++ Computational_physics/quantum_wavefunction/functions.jl
@@ -2,8 +2,8 @@
 explaining the numbers in my report. =#
 
 module Functions
-
-# Creating Coordinate function
+export grid, righthandside, gauss_quad_2D
+# Creating Coordinate/Mesh function, returns grid ,l2g matrix and number of elements / nodes.
 function grid(domain::Tuple{Real, Real}, max_length::Float64) # num_of_squares_x::UInt16, num_of_squares_y::UInt16
 
     # Define domain length in any axis since x and y have the same domain
@@ -86,6 +86,94 @@ function grid(domain::Tuple{Real, Real}, max_length::Float64) # num_of_squares_x
             index += 1
         end
     end
-    return l2g
+    return coords, l2g ,noe, nop
+end
+# Creating function which will be returning the stiffness matrix and the right hand side of the system with F.E.M
+function righthandside(coords::Matrix{Float64}, l2g::Matrix{Int64}, noe::Int64, nop::Int64) # Can just put one variable results = Functions.grid(domain, max_length) in main
+
+    # Assemble stiffness matrix indices (triplets for sparse matrix)
+    ia = zeros(noe * 16, 1); # Row index, noe * 16 because the two for loops for each square = 16 iterations for each element --> noe * 16
+    ja = zeros(noe * 16, 1); # Column index
+    va = zeros(noe * 16, 1); # Value index
+
+    # Initialize global force vector
+    F = zeros(nop, 1);
+
+    # Shape/basis functions for integration
+    N1(ksi, eta) = 1/4 * (1 - ksi) * (1 - eta)
+    N2(ksi, eta) = 1/4 * (1 + ksi) * (1 - eta)
+    N3(ksi, eta) = 1/4 * (1 + ksi) * (1 + eta)
+    N4(ksi, eta) = 1/4 * (1 - ksi) * (1 + eta)
+
+    # Iterate over elements to find global stiffness and force matrices
+    index = 1
+    for e = 1:noe
+        xe = coords[l2g[e, :], 1]
+        ye = coords[l2g[e, :], 2]
+
+        # Creating jacobian matrix for element e
+        J11(ksi, eta) = 0.25 * (- (1 - eta) * xe[1] + (1 - eta) * xe[2] + (1 + eta) * xe[3] - (1 + eta) * xe[4])
+        J21(ksi, eta) = 0.25 * (- (1 - ksi) * xe[1] - (1 + ksi) * xe[2] + (1 + ksi) * xe[3] + (1 - ksi) * xe[4])
+        J12(ksi, eta) = 0.25 * (- (1 - eta) * ye[1] + (1 - eta) * ye[2] + (1 + eta) * ye[3] - (1 + eta) * ye[4])
+        J22(ksi, eta) = 0.25 * (- (1 - ksi) * ye[1] - (1 + ksi) * ye[2] + (1 + ksi) * ye[3] + (1 - ksi) * ye[4])
+
+        detJ(ksi, eta) = J11(ksi, eta) * J22(ksi, eta) - J12(ksi, eta) * J21(ksi, eta)
+
+        # Define θNx and θNy as arrays of functions, will not include 1/detJ(ksi, eta) since it is multiplied by the same function anyway so i will just / in the integrand 
+        #for efficiency instead of calculating it 8 times here and / it another 48 times
+        θNx = [
+            (ksi, eta) -> 0.25 * (J22(ksi, eta) * (eta - 1) + J12(ksi, eta) * (ksi - 1)),
+            (ksi, eta) -> 0.25 * (-J22(ksi, eta) * (eta - 1) + J12(ksi, eta) * (ksi + 1)),
+            (ksi, eta) -> 0.25 * (J22(ksi, eta) * (eta + 1) - J12(ksi, eta) * (ksi + 1)),
+            (ksi, eta) -> 0.25 * (-J22(ksi, eta) * (eta + 1) - J12(ksi, eta) * (ksi - 1))
+        ]
+        θNy = [
+            (ksi, eta) -> 0.25 * (J21(ksi, eta) * (1 - eta) - J11(ksi, eta) * (1 - ksi)),
+            (ksi, eta) -> 0.25 * (-J21(ksi, eta) * (1 - eta) - J11(ksi, eta) * (1 + ksi)),
+            (ksi, eta) -> 0.25 * (-J21(ksi, eta) * (1 + eta) + J11(ksi, eta) * (1 + ksi)),
+            (ksi, eta) -> 0.25 * (J21(ksi, eta) * (1 + eta) + J11(ksi, eta) * (1 - ksi))
+        ]
+
+        # Precompute the local stiffness matrix M with αx and αy equal to 1 for this project, for more advanced application this could change
+        M = zeros(4, 4)
+        for i = 1:4
+            for j = 1:4
+                integrand(ksi, eta) = -(θNx[i](ksi, eta) * θNx[j](ksi, eta) + θNy[i](ksi, eta) * θNy[j](ksi, eta)) / detJ(ksi, eta)
+                M[i, j] = gauss_quad_2D(integrand, 2)
+            end
+        end
+
+        # Assemble global stiffness matrix from the local element matrices
+        for i = 1:4
+            for j = 1:4
+                ia[index] = l2g[e, i] # global node index at row i of local matrix
+                ja[index] = l2g[e, j] # global node index at collumn j
+                va[index] = M[i, j] # value of local stiffness matrix at i,j
+                index = index + 1;
+            end
+        end
+    end
+    return va
+end
+# Creating 2 dimensional gauss_quadrature function to use to solve the integrals needed in FEM integrating from -1 to 1
+function gauss_quad_2D(funct, n)
+    if n == 2
+        ksi = (-0.5773502692, 0.5773502692)
+        eta = (-0.5773502692, 0.5773502692)
+        w = ones(2)
+    elseif n == 3
+        ksi = (0, -0.7745966692, 0.7745966692)
+        eta = (0, -0.7745966692, 0.7745966692)
+        w = (0.8888888889, 0.5555555556, 0.5555555556)
+    else
+        error("Only n = 2 or n = 3 are supported")
+    end
+    result = 0.0
+    for i = 1:n
+        for j = 1:n
+            result += w[i] * w[j] * funct(ksi[i], eta[j])
+        end
+    end
+    return result
 end
 end # Module end
--- Computational_physics/quantum_wavefunction/test.jl
+++ Computational_physics/quantum_wavefunction/test.jl
@@ -4,5 +4,12 @@ import .Functions  # Use the module (note the dot '.')
 domain = (-1, 1)
 max_length = 1.0
 
-results = Functions.grid(domain, max_length)  # Call the function
-println(results) # Have to create seperate function for solution and seperate for grid
+results = Functions.grid(domain, max_length)  # Get tuple 
+results_1 = Functions.righthandside(results...) # Splat the tuple
+println(results_1) # Have to create seperate function for solution and seperate for grid
+#f = (x, y) -> x^2 + y^2  # Anonymous function
+#approx = Functions.gauss_quad_2D(f, 2)
+#println("Approximation for n=2: ", approx)
+
+#approx = Functions.gauss_quad_2D((θ, η) -> sin(θ) + cos(η), 3)  # Inline anonymous function
+#println("Approximation for n=3: ", approx)
